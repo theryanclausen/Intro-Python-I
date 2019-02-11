e9dbabe81336fbfbf1ccefd814d3bc558a192a7c
theryanclausen <theryanclausen@gmail.com>
2019-02-11 15:24:08 -0700

    hello complete. progress on args

--- src/args.py
+++ src/args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num, num2):
+    return (num + num2)
+
 
 print(f1(1, 2))
 
@@ -13,6 +16,12 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+def f2(*nums):
+    result = 0
+    for num in nums:
+        result += num
+    return result
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -20,14 +29,16 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+#What thing do you have to add to make this work?
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2 = 1):
+    return(num1 + num2)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -46,18 +57,18 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+#f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+#f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
+'''d = {
     "monster": "goblin",
     "hp": 3
-}
+}'''
 
 # What thing do you have to add to make this work?
-f4(d)
+#f4(d)
